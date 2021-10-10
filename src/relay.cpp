09c857a26d419279d9910f5c682d9be24f88dfbf
Mateusz Lichota <mateusz@lichota.net>
2021-10-10 11:25:26 +0100

    refactor: extract functions and parameters
    Signed-off-by: Mateusz Lichota <mateusz@lichota.net>

--- src/relay.cpp
+++ src/relay.cpp
@@ -14,21 +14,36 @@ class RelayNode : public rclcpp::Node {
     RelayNode();
 
   private:
+    void incoming_message_callback(std::shared_ptr<rclcpp::SerializedMessage> msg);
+    void subscribe();
+    void unsubscribe();
+
     rclcpp::GenericSubscription::SharedPtr sub_;
     rclcpp::GenericPublisher::SharedPtr pub_;
+    std::string input_topic;
+    std::string output_topic;
+    std::string topic_type;
 };
 
 RelayNode::RelayNode() : rclcpp::Node("Relay") {
-  std::string input_topic = declare_parameter<std::string>("input_topic");
-  std::string output_topic = declare_parameter<std::string>("output_topic", input_topic + "_relay");
-  std::string topic_type = declare_parameter<std::string>("type");
+  input_topic = declare_parameter<std::string>("input_topic");
+  output_topic = declare_parameter<std::string>("output_topic", input_topic + "_relay");
   pub_ = this->create_generic_publisher(output_topic, topic_type, rclcpp::QoS(1));
-  sub_ = this->create_generic_subscription(
-    input_topic, topic_type, rclcpp::QoS(1),
-    [this](std::shared_ptr<rclcpp::SerializedMessage> msg) {
-      rclcpp::SerializedMessage msg_rcl = *msg.get();
-      pub_->publish(msg_rcl);
-    });
+
+  subscribe();
+}
+
+void RelayNode::subscribe() {
+  sub_ = this->create_generic_subscription(input_topic, topic_type, rclcpp::QoS(1), std::bind(&RelayNode::incoming_message_callback, this, std::placeholders::_1));
+}
+
+void RelayNode::unsubscribe() {
+  if (sub_) sub_.reset();
+}
+
+void RelayNode::incoming_message_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) {
+  rclcpp::SerializedMessage msg_rcl = *msg.get();
+  pub_->publish(msg_rcl);
 }
 
 }  // namespace topic_tools
