1e77c5b2b56abd1390f9a9bbae4a73188d07980f
Mateusz Lichota <mateusz@lichota.net>
2021-10-10 16:19:13 +0100

    allow dynamically finding the topic type
    Signed-off-by: Mateusz Lichota <mateusz@lichota.net>

--- src/relay.cpp
+++ src/relay.cpp
@@ -6,6 +6,8 @@
 #include <memory>
 #include <string>
 
+using namespace std::chrono_literals;
+
 namespace topic_tools
 {
 
@@ -15,38 +17,60 @@ class RelayNode : public rclcpp::Node {
 
   private:
     void incoming_message_callback(std::shared_ptr<rclcpp::SerializedMessage> msg);
-    void subscribe();
+    void timer_callback();
     void unsubscribe();
+    std::optional<std::string> try_find_topic_type();
 
     rclcpp::GenericSubscription::SharedPtr sub_;
     rclcpp::GenericPublisher::SharedPtr pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
     std::string input_topic;
     std::string output_topic;
-    std::string topic_type;
+    std::optional<std::string> topic_type;
 };
 
 RelayNode::RelayNode() : rclcpp::Node("Relay") {
   input_topic = declare_parameter<std::string>("input_topic");
   output_topic = declare_parameter<std::string>("output_topic", input_topic + "_relay");
 
-  auto topic_names_and_types = this->get_topic_names_and_types();
-  auto topic_type_i = topic_names_and_types.find(input_topic);
+  auto ros_clock = rclcpp::Clock::make_shared();
+  timer_ = rclcpp::create_timer(this, ros_clock, 1s, [=](){ timer_callback(); });
 
-  if (topic_type_i == topic_names_and_types.end()) {
-    std::cout << "error: input topic doesn't exist" << std::endl;
-    return;
-  }
-
-  topic_type = topic_type_i->second[0];
-  
-
-  pub_ = this->create_generic_publisher(output_topic, topic_type, rclcpp::QoS(1));
-
-  subscribe();
+  // so that we execute the loop the first time instantly
+  timer_callback();
 }
 
-void RelayNode::subscribe() {
-  sub_ = this->create_generic_subscription(input_topic, topic_type, rclcpp::QoS(1), std::bind(&RelayNode::incoming_message_callback, this, std::placeholders::_1));
+void RelayNode::timer_callback() {
+  if (!topic_type) {
+    if (topic_type = try_find_topic_type()) {
+      // we just found the topic type, so create the publisher and the subscriber
+      pub_ = this->create_generic_publisher(output_topic, *topic_type, rclcpp::QoS(1));
+      sub_ = this->create_generic_subscription(input_topic, *topic_type, rclcpp::QoS(1), std::bind(&RelayNode::incoming_message_callback, this, std::placeholders::_1));
+    }
+  }
+}
+
+/*
+Since we don't know the topic type in advance we need to determine it from
+either the input_topic or the output_topic. If only one of them exists, we
+use its type, but if both exist an arbitrary decision was made here to use
+the type of input_topic.
+*/
+std::optional<std::string> RelayNode::try_find_topic_type() {
+  auto const topic_names_and_types = this->get_topic_names_and_types();
+  auto topic_type_i = topic_names_and_types.find(input_topic);
+
+  if (topic_type_i != topic_names_and_types.end()) {
+    return topic_type_i->second[0];
+  }
+
+  topic_type_i = topic_names_and_types.find(output_topic);
+
+  if (topic_type_i != topic_names_and_types.end()) {
+    return topic_type_i->second[0];
+  }
+
+  return {};
 }
 
 void RelayNode::unsubscribe() {
