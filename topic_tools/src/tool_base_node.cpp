71567ed3c5ff994fc2ceb86839a99de4ba7e2e18
Steve Nogar <stephen.m.nogar.civ@mail.mil>
2021-12-21 21:02:31 -0500

    improve qos detection robustness
    Signed-off-by: Steve Nogar <stephen.m.nogar.civ@mail.mil>

--- topic_tools/src/tool_base_node.cpp
+++ topic_tools/src/tool_base_node.cpp
@@ -59,10 +59,73 @@ void ToolBaseNode::make_subscribe_unsubscribe_decisions()
 
 std::optional<std::pair<std::string, rclcpp::QoS>> ToolBaseNode::try_discover_source()
 {
-  auto publishers_info = this->get_publishers_info_by_topic(input_topic_);
-  std::optional<rclcpp::QoS> qos_profile;
-  if (!publishers_info.empty()) {
-    return std::make_pair(publishers_info[0].topic_type(), publishers_info[0].qos_profile());
+  // borrowed this from domain bridge 
+  // (https://github.com/ros2/domain_bridge/blob/main/src/domain_bridge/wait_for_graph_events.hpp)
+  
+  // Query QoS info for publishers
+  std::vector<rclcpp::TopicEndpointInfo> endpoint_info_vec =
+    this->get_publishers_info_by_topic(input_topic_);
+  std::size_t num_endpoints = endpoint_info_vec.size();
+
+  // If there are no publishers, return an empty optional
+  if (num_endpoints < 1u) {
+    return {};
+  }
+
+  // Initialize QoS
+  rclcpp::QoS qos{10};
+  // Default reliability and durability to value of first endpoint
+  qos.reliability(endpoint_info_vec[0].qos_profile().reliability());
+  qos.durability(endpoint_info_vec[0].qos_profile().durability());
+  // Always use automatic liveliness
+  qos.liveliness(rclcpp::LivelinessPolicy::Automatic);
+
+  // Reliability and durability policies can cause trouble with enpoint matching
+  // Count number of "reliable" publishers and number of "transient local" publishers
+  std::size_t reliable_count = 0u;
+  std::size_t transient_local_count = 0u;
+  // For duration-based policies, note the largest value to ensure matching all publishers
+  rclcpp::Duration max_deadline(0, 0u);
+  rclcpp::Duration max_lifespan(0, 0u);
+  for (const auto & info : endpoint_info_vec) {
+    const auto & profile = info.qos_profile();
+    if (profile.reliability() == rclcpp::ReliabilityPolicy::Reliable) {
+      reliable_count++;
+    }
+    if (profile.durability() == rclcpp::DurabilityPolicy::TransientLocal) {
+      transient_local_count++;
+    }
+    if (profile.deadline() > max_deadline) {
+      max_deadline = profile.deadline();
+    }
+    if (profile.lifespan() > max_lifespan) {
+      max_lifespan = profile.lifespan();
+    }
+  }
+
+  // If not all publishers have a "reliable" policy, then use a "best effort" policy
+  // and print a warning
+  if (reliable_count > 0u && reliable_count != num_endpoints) {
+    qos.best_effort();
+    RCLCPP_WARN(this->get_logger(), "Some, but not all, publishers on topic %s "
+      "offer 'reliable' reliability. Falling back to 'best effort' reliability in order"
+      "to connect to all publishers.",input_topic_.c_str());
+  }
+
+  // If not all publishers have a "transient local" policy, then use a "volatile" policy
+  // and print a warning
+  if (transient_local_count > 0u && transient_local_count != num_endpoints) {
+    qos.durability_volatile();
+    RCLCPP_WARN(this->get_logger(), "Some, but not all, publishers on topic %s "
+      "offer 'transient local' durability. Falling back to 'volatile' durability in order"
+      "to connect to all publishers.",input_topic_.c_str());
+  }
+
+  qos.deadline(max_deadline);
+  qos.lifespan(max_lifespan);
+
+  if (!endpoint_info_vec.empty()) {
+    return std::make_pair(endpoint_info_vec[0].topic_type(), qos);
   } else {
     return {};
   }
