91508fc5dbead212ea9482202f4b4e167eb8b328
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2022-01-24 11:26:51 +0100

    Add delay tool
    Signed-off-by: Marcel Zeilinger <marcel.zeilinger@ait.ac.at>

---
+++ src/delay_node.cpp
@@ -0,0 +1,44 @@
+// Copyright 2022 AIT Austrian Institute of Technology GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <utility>
+#include "rclcpp/rclcpp.hpp"
+#include "topic_tools/delay_node.hpp"
+
+namespace topic_tools
+{
+using namespace std::chrono_literals; // NOLINT
+
+DelayNode::DelayNode(const rclcpp::NodeOptions & options)
+: ToolBaseNode("delay", options), delay_(0ns)
+{
+  delay_ = rclcpp::Duration::from_seconds(declare_parameter("delay", 0.0));
+  use_wall_clock_ = declare_parameter("use_wall_clock", false);
+}
+
+void DelayNode::process_message(std::shared_ptr<rclcpp::SerializedMessage> msg)
+{
+  auto now = use_wall_clock_ ? rclcpp::Clock{}.now() : this->now();
+  queue_.push_back({ now + delay_, msg });
+  while (!queue_.empty() && queue_.front().first <= now) {
+    pub_->publish(*queue_.front().second);
+    queue_.pop_front();
+  }
+}
+
+}  // namespace topic_tools
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(topic_tools::DelayNode)
