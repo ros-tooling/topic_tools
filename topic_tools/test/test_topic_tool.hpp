55702dd4653ff047cd029e4d0a843f4294b3abfa
Martin Llofriu <mllofriu@gmail.com>
2023-05-05 16:22:57 -0300

    Add a test for mux. Add a call to re-suscribe when the topic is switched
    Signed-off-by: Martin Llofriu <mllofriu@gmail.com>

--- topic_tools/test/test_topic_tool.hpp
+++ topic_tools/test/test_topic_tool.hpp
@@ -16,6 +16,7 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "gtest/gtest.h"
 #include "rclcpp/rclcpp.hpp"
@@ -99,5 +100,69 @@ private:
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   std::string target_input_topic_;
+  std::string target_output_topic_;
+};
+
+class TestTopicToolMultiSub : public TestTopicTool
+{
+public:
+  void SetUp()
+  {
+    using std::placeholders::_1;
+    const std::string test_name =
+      ::testing::UnitTest::GetInstance()->current_test_info()->name();
+    test_node_ = rclcpp::Node::make_shared(test_name);
+    target_input_topic_prefix_ = "/" + test_name + "/input";
+    target_output_topic_ = "/" + test_name + "/output";
+    subscription_ = test_node_->create_subscription<std_msgs::msg::String>(
+      target_output_topic_, 10, std::bind(&TestTopicToolMultiSub::topic_callback, this, _1));
+    std::vector<std::string> topic_names = get_target_input_topics();
+    for (size_t i = 0; i < topic_names.size(); i++) {
+      publishers_[i] = test_node_->create_publisher<std_msgs::msg::String>(
+        topic_names[i], 10);
+    }
+  }
+  void TearDown()
+  {
+    test_node_.reset();
+    subscription_.reset();
+    for (int i = 0; i < num_target_input_topics_; i++) {
+      publishers_[i].reset();
+    }
+  }
+  void publish_and_check(
+    std::string msg_content,
+    int publisher_index,
+    std::shared_ptr<rclcpp::Node> target_node)
+  {
+    auto message = std_msgs::msg::String();
+    message.data = msg_content;
+    assert(publisher_index < num_target_input_topics_);
+    publishers_[publisher_index]->publish(message);
+    rclcpp::spin_some(target_node);
+    rclcpp::spin_some(test_node_);
+  }
+  std::vector<std::string> get_target_input_topics()
+  {
+    std::vector<std::string> res;
+    for (int i = 0; i < num_target_input_topics_; i++) {
+      res.push_back(target_input_topic_prefix_ + std::to_string(i));
+    }
+    return res;
+  }
+  std::string get_target_output_topic()
+  {
+    return target_output_topic_;
+  }
+
+protected:
+  std::shared_ptr<rclcpp::Node> test_node_;
+
+private:
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  static constexpr int num_target_input_topics_ = 3;
+  std::array<rclcpp::Publisher<std_msgs::msg::String>::SharedPtr,
+    num_target_input_topics_> publishers_;
+  std::string target_input_topic_prefix_;
   std::string target_output_topic_;
 };
